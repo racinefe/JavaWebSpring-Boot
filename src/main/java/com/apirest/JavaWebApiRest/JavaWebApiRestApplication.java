a2fd0196882e3dbfc9c0eac8a15afa0808ac826a
@racinefell <102471647+racinefe@users.noreply.github.com>
2023-11-06 11:11:29 -0300

    projeto Spring Boot
    criando e configurando projeto Spring Boot

---
+++ src/main/java/com/apirest/JavaWebApiRest/JavaWebApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.JavaWebApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApiRestApplication.class, args);
+	}
+
+}
