057781303b97b579c21a1584f7e36e07a2618244
@racinefell <102471647+racinefe@users.noreply.github.com>
2023-11-07 14:57:10 -0300

    Modelo de domínio
    criando e associando a classe "Department" com a classe "User"

---
+++ src/main/java/com/apirest/JavaWebApiRest/entitties/Department.java
@@ -0,0 +1,27 @@
+package com.apirest.JavaWebApiRest.entitties;
+
+public class Department {
+	private long id;
+	private String name;
+	
+	public Department(){
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
